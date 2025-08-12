30a3f48d0b6d047672d244c936c1a17b19460f8c
shravan28281 <shrvanre+1@gmail.com>
2025-08-12 10:18:27 -0700

    Update Main.java

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -3,6 +3,7 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
+    World.setDelay(0); // remove after.
 
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
@@ -64,6 +65,62 @@ public class Main implements Directions{
     r.putBeeper();
     r.turnLeft();
     r.turnLeft(); 
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+   r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
     r.move();
     r.putBeeper();
 
